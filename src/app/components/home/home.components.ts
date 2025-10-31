c8b823aaa8267b1881a453b28b48cc3764da0a6b
pedrodomingosdev <pedrodomingos.profissional@gmail.com>
2025-10-31 11:03:12 -0300

    feat: Adicição do botão "Remover"

--- src/app/components/home/home.components.ts
+++ src/app/components/home/home.components.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home.components',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.components.html',
   styleUrl: './home.components.css'
 })
 export class HomeComponents {
+  novoProduto = '';           // texto digitado
+  listaProdutos: string[] = [];  // lista de itens
 
+  incluirProdutos() {
+    if (this.novoProduto.trim()) {
+      this.listaProdutos.push(this.novoProduto.trim());
+    }
+  }
+
+  removerProdutos() {
+    const valor = this.novoProduto.trim();
+    this.listaProdutos = this.listaProdutos.filter(p => p !== valor);
+  }
 }
