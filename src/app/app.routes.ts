3a34a16a254c5470ce3c4eba6826c813e3e4233f
pedrodomingosdev <pedrodomingos.profissional@gmail.com>
2025-10-31 10:02:08 -0300

    feat: Inicio do Projeto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { HomeComponents } from "./components/home/home.components";
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponents
+    }
+];
---
+++ src/app/components/home/home.components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home.components',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.components.html',
+  styleUrl: './home.components.css'
+})
+export class HomeComponents {
+
+}
