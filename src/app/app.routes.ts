af21b5f8b46eeee439d332592b00d79de9a935e4
pedrodomingosdev <pedrodomingos.profissional@gmail.com>
2025-11-01 21:43:42 -0300

    feat: Remoção de Arquivos a mais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponents } from './components/home/home.components';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponents
+    }
+];
